8c766336642ce29be2ec411b36ed64fc69bca38e
ruzomkrtchyan <ruzanna.mkrtchyan.01@gmail.com>
2025-01-18 18:37:53 +0400

    corrected

--- main.cpp
+++ main.cpp
@@ -2,18 +2,26 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <cstdlib>
+#include <fcntl.h>
 
 int main(int argc, char **argv)
 {
+    if (argc != 3)
+    {
+        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+        return(1);
+    }
+    
     int port_n = std::atoi(argv[1]);
     int sockfd = socket(AF_INET, SOCK_STREAM, 0); // creates an endpoint for communication
     if (sockfd == -1)
     {
         std::cerr << "Failed to create a socket!" << std::endl;
-        return(-1); 
+        return(1); 
     }
 
     // Need to to set the socket to non-blocking
+
     struct sockaddr_in serv_addr;
     std::memset(&serv_addr, 0, sizeof(serv_addr));
 
@@ -21,17 +29,17 @@ int main(int argc, char **argv)
     serv_addr.sin_port = htons(port_n);
     inet_pton(AF_INET, "0.0.0.0", &serv_addr.sin_addr);
 
-    if (bind(sockfd, AF_INET, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
     {
         std::cerr << "Failed to bind a socket!" << std::endl;
         close(sockfd);
-        return(-1);
+        return(1);
     }
     if (listen(sockfd, SOMAXCONN) == -1)
     {
         std::cerr << "Failed to listen!" << std::endl;
         close(sockfd);
-        return(-1);
+        return(1);
     }
 
     struct sockaddr_in client_addr;
@@ -42,7 +50,7 @@ int main(int argc, char **argv)
     {
         std::cerr << "Failed to accept client connection!" << std::endl;
         close(sockfd);
-        return(-1);
+        return(1);
     }
     close(sockfd);
 }
