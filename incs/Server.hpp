4ac25df9f62dd5d1c56312ae5ae896e9d6bc1162
Armine Hakobyan <armhakob@c2r3s5.42yerevan.am>
2025-02-02 21:14:05 +0400

    Makefile

---
+++ incs/Server.hpp
@@ -0,0 +1,35 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <sys/socket.h>
+#include <cstdlib>
+#include <fcntl.h>
+#include <poll.h>
+#include <arpa/inet.h>
+#include <vector>
+#include <unistd.h>
+#include <map>
+
+class Client;
+class Channel;
+
+class Server
+{
+    private:
+        int         port;
+        int         sockfd;
+        std::string password;
+        std::vector<struct pollfd> fds;
+
+        void create_sock();
+        void new_client();
+        void receiving_data(int i);
+
+    public:
+        Server(int prt, std::string passw);
+        void connect();
+
+};
+
+#endif
