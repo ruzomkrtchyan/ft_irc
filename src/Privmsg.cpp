823b1bb965c240dac993a15069e27a53db8a19b4
ruzomkrtchyan <ruzanna.mkrtchyan.01@gmail.com>
2025-03-13 16:08:15 +0400

    Privmsg

--- src/Privmsg.cpp
+++ src/Privmsg.cpp
@@ -10,10 +10,34 @@ void Privmsg::execute(Server &serv, Client &client, const std::vector<std::strin
 {
     if (args.size() < 3)
     {
-        client.sendMessage(ERR_NEEDMOREPARAMS(" ", "PRIVMSG"));
+        client.sendMessage(ERR_NEEDMOREPARAMS(client.getNickname(), "PRIVMSG"));
+        return;
+    }
+
+    std::string target = args[1]
+    if (target[0] == "#")
+    {
+        if (serv._channels.find(target) == server._channels.end())
+        {
+            client.sendMessage(ERR_NOSUCHCHANNEL(client.getNickname(), "PRIVMSG"));
+            return;
+        }
+
+        Channel chn = serv._channels[target];
+     
+        if (!chn.isMember(client))
+        {
+            client.sendMessage(ERR_NOTONCHANNEL(client.getNickname(), "PRIVMSG"));
+            return;
+        }
+
+        std::string msg = client.getNickname() + " PRIVMSG " + target +;
     }
 }
 
 PRIVMSG <target> :<message>
 <target> → A nickname (to message a specific user) or a channel (to message a group).
 <message> → The actual text message.
+
+PRIVMSG #general :Hello everyone!
+:user1 PRIVMSG #general :Hello everyone!
